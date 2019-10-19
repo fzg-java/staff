6759168f9a6058fca42f41f2c4a0bf83a1363454
fzg394 <1402731930@qq.com>
2019-10-19 09:27:46 +0800

    gittub 项目搭建

---
+++ gittub/src/main/java/com/lanou/gittub/GittubApplication.java
@@ -0,0 +1,13 @@
+package com.lanou.gittub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GittubApplication.class, args);
+    }
+
+}
